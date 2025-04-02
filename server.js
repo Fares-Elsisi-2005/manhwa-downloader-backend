262f65a53932783ec4bcb7c3e49e9c1a74812c48
Fares <fareselsisi35@gmail.com>
2025-04-02 03:56:14 +0200

    Allows any origin

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const axios = require("axios");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*", // Allows any origin
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.static(__dirname));
 
 let activeRequests = 0;
